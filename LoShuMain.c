90a2a09cb5d6d77edf231b0e1ef02bd9828a59cc
Justin Mendez <jmmendez@cpp.edu>
2021-10-01 23:58:31 -0700

    commit

---
+++ LoShuMain.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//Size of 2d array
+const int rows = 3;
+const int cols = 3;
+
+
+int main(){
+    int magicSquare[3][3];
+    magicSquare[3][3] = {{4,9,2},{3,5,7},{8,1,6}};
+
+    return 0;
+}
+
+
